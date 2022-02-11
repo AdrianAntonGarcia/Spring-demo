61856a5061bd2c1f39aba6800e1f7f4b611a1844
Adrián Antón <adrtler@gmail.com>
2022-02-11 13:21:14 +0100

    Pasar datos a la vista

--- src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
@@ -1,18 +1,44 @@
 package com.bolsaideas.springboot.web.app.controllers;
 
+//import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+//import org.springframework.ui.ModelMap;	
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 	
 	/* por defecto es GET */
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String index() {
-		return "index";
+	
+	@RequestMapping(value = {"/index", "/"}, method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mv) {
+		mv.addObject("titulo", "hola spring framework con modelAndView!");
+		mv.setViewName("index");
+		return mv;
 	}
+	
+	// Diferentes formas de hacer lo mismo
+//	@RequestMapping(value = {"/index", "/"}, method = RequestMethod.GET)
+//	public String index(Map<String, Object> model) {
+//		model.put("titulo", "hola spring framework con map!");
+//		return "index";
+//	}
+//	
+	
+//	@RequestMapping(value = {"/index", "/"}, method = RequestMethod.GET)
+//	public String index(ModelMap model) {
+//		model.addAttribute("titulo", "hola spring framework dato");
+//		return "index";
+//	}
+	
+	
+	///////////////////////////
+	
+	
 	
 //	@GetMapping("/index")
 //	@GetMapping(value = "/index")
