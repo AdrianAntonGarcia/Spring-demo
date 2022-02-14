ba129a222a256816205eddf78812635b49d5123a
Adrián Antón <adrtler@gmail.com>
2022-02-14 09:24:02 +0100

    El objeto model

--- src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsaideas/springboot/web/app/controllers/IndexController.java
@@ -3,25 +3,37 @@ package com.bolsaideas.springboot.web.app.controllers;
 //import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 //import org.springframework.ui.ModelMap;	
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bolsaideas.springboot.web.app.models.Usuario;
+
 @Controller
 @RequestMapping("/app")
 public class IndexController {
-	
+
 	/* por defecto es GET */
-	
-	@RequestMapping(value = {"/index","", "/", "home"}, method = RequestMethod.GET)
+
+	@RequestMapping(value = { "/index", "", "/", "home" }, method = RequestMethod.GET)
 	public ModelAndView index(ModelAndView mv) {
 		mv.addObject("titulo", "hola spring framework con modelAndView!");
 		mv.setViewName("index");
 		return mv;
 	}
-	
+
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Adrián");
+		usuario.setApellido("Antón");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		return "perfil";
+	}
 	// Diferentes formas de hacer lo mismo
 //	@RequestMapping(value = {"/index", "/"}, method = RequestMethod.GET)
 //	public String index(Map<String, Object> model) {
@@ -29,18 +41,15 @@ public class IndexController {
 //		return "index";
 //	}
 //	
-	
+
 //	@RequestMapping(value = {"/index", "/"}, method = RequestMethod.GET)
 //	public String index(ModelMap model) {
 //		model.addAttribute("titulo", "hola spring framework dato");
 //		return "index";
 //	}
-	
-	
+
 	///////////////////////////
-	
-	
-	
+
 //	@GetMapping("/index")
 //	@GetMapping(value = "/index")
 //	public String index() {
